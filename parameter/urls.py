7b7b7988fdad2e859890c78fdc861b188cadeab5
hy <h-y22@mails.tsinghua.edu.cn>
2024-07-10 19:34:57 +0800

    add parameter.urls.py

--- parameter/urls.py
+++ parameter/urls.py
@@ -1,11 +1,30 @@
 from django.urls import path
-import user.views as views
+import parameter.views as views
+
 urlpatterns = [
-    path('login', views.login, name='login'),
-    path('signup', views.signup, name='signup'),
-    # path('jwt',views.refresh_access_token, name='refresh_token'),
-    path('logout',views.logout, name='logout'),
-    path('cancel',views.cancel, name='cancel'),
-    path('info',views.info, name='info'),
-    path('change_password',views.change_password, name='change_password')
+    path('start_site/', views.start_site, name='start_site'),
+    path('del_start_site/', views.del_start_site, name='del_start_site'),
+    path('start_site_list/<int:per_page>/<int:page>/', views.start_site_list, name='start_site_list'),
+    
+    path('end_site/', views.end_site, name='end_site'),
+    path('del_end_site/', views.del_end_site, name='del_end_site'),
+    path('end_site_list/<int:per_page>/<int:page>/', views.end_site_list, name='end_site_list'),
+
+    path('new_goods/', views.new_goods, name='new_goods'),
+    path('del_goods/', views.del_goods, name='del_goods'),
+    path('goods_list/<int:per_page>/<int:page>/', views.goods_list, name='goods_list'),
+
+    path('new_vehicle/', views.new_vehicle, name='new_vehicle'),
+    path('del_vehicle/', views.del_vehicle, name='del_vehicle'),
+    path('vehicle_list/<int:per_page>/<int:page>/', views.vehicle_list, name='vehicle_list'),
+
+    path('new_site2owner/', views.new_site2owner, name='new_site2owner'),
+    path('del_site2owner/', views.del_site2owner, name='del_site2owner'),
+    path('site2owner_list/<int:per_page>/<int:page>/', views.site2owner_list, name='site2owner_list'),
+    path('owner2site/<int:per_page>/<int:page>/', views.owner2site, name='owner2site'),
+    path('site2owner/', views.site2owner, name='site2owner_detail'),
+
+    path('new_pay/', views.new_pay, name='new_pay'),
+    path('del_pay/', views.del_pay, name='del_pay'),
+    path('pay_list/<int:per_page>/<int:page>/', views.pay_list, name='pay_list'),
 ]
